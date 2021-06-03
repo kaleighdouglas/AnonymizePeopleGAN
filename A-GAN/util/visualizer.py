7a20f23ba188d5414f4d741063899bfe6499024d
Kaleigh O'Hara <>
2021-06-03 16:21:48 +0200

    minor changes

--- A-GAN/util/visualizer.py
+++ A-GAN/util/visualizer.py
@@ -264,13 +264,13 @@ class ValidationVisualizer():
         Step 3: create an HTML object for saveing HTML filters
         """
 
-        opt.phase = 'val'
-        opt.num_threads = 0   # test code only supports num_threads = 0
-        # opt.batch_size = 1    # test code only supports batch_size = 1
-        opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
-        # opt.no_flip = True    # no flip; comment this line if results on flipped images are needed.
-        # opt.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
-        # opt.max_dataset_size = 2
+        # opt.phase = 'val'
+        # opt.num_threads = 0   # test code only supports num_threads = 0
+        # # opt.batch_size = 1    # test code only supports batch_size = 1
+        # opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
+        # # opt.no_flip = True    # no flip; comment this line if results on flipped images are needed.
+        # # opt.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
+        # # opt.max_dataset_size = 2
 
         self.opt = opt  # cache the option
         self.display_id = opt.display_id
